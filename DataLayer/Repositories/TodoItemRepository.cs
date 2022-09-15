8f6386c05b33dfe4a4d2e7809d50dfddddff9bfb
Toni Škobić <toni.skobic@gmail.com>
2022-09-16 00:21:08 +0200

    - IService implemented - IUserService changes made

--- DataLayer/Repositories/TodoItemRepository.cs
+++ DataLayer/Repositories/TodoItemRepository.cs
@@ -5,8 +5,18 @@ namespace DataLayer.Repositories
 {
     public class TodoItemRepository : DataRepository<TodoItem>, ITodoItemRepository
     {
+        private readonly TodoContext _todoContext;
         public TodoItemRepository(TodoContext todoContext) : base(todoContext)
         {
+            _todoContext = todoContext;
+        }
+
+        public void Update(TodoItem todoItemToUpdate, TodoItem todoItem)
+        {
+
+            todoItemToUpdate.Name = todoItem.Name;
+            todoItemToUpdate.IsComplete = todoItem.IsComplete;
+            _todoContext.SaveChanges();
         }
     }
 }
