a39cc615b769542e72c06b29f8179ca36057754f
tskobic <toni.skobic@gmail.com>
2022-09-15 16:50:27 +0200

    TodoAPI: - created Business and Data Layer projects

---
+++ DataLayer/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using DataLayer.Models;
+
+namespace DataLayer
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
---
+++ DataLayer/TodoContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class TodoContextFactory : IDesignTimeDbContextFactory<TodoContext>
+    {
+        public TodoContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
+            optionsBuilder.UseInMemoryDatabase("Todo");
+
+            return new TodoContext(optionsBuilder.Options);
+        }
+    }
+}
